4f82efc4cfd98f22e07685ef4ce98c1cd366c04f
Milton Montero <lleramontero@gmail.com>
2026-04-29 19:45:19 +0200

    Fix amodal completion.

--- mindset/generators/low_mid_vision/amodal_completion.py
+++ mindset/generators/low_mid_vision/amodal_completion.py
@@ -128,18 +128,19 @@ class AmodalCompletionConfig(GeneratorConfig):
 @generator(AmodalCompletionConfig)
 def generate_all(config: AmodalCompletionConfig):
     """generate amodal completion dataset."""
+
+    canvas_size = config.canvas_size
     output_folder = Path(config.output_folder)
+
     ds = DrawCompletion(
-        background=config.background_color,
-        canvas_size=config.canvas_size,
+        background=config.background_color,  # type: ignore
+        canvas_size=canvas_size,  # type: ignore
         antialiasing=config.antialiasing,
     )
 
-    for cond in ["occlusion", "no_occlusion", "notched"]:
+    for cond in ["occluded", "non_occluded", "notched"]:
         (output_folder / cond).mkdir(exist_ok=True, parents=True)
 
-    top_shapes = ["s", "c"]
-    canvas_size = config.canvas_size
 
     check_square_fully_in_canvas = lambda cs: (
         cs[0] - side_square // 2 > 0
@@ -147,22 +148,16 @@ def generate_all(config: AmodalCompletionConfig):
         and cs[1] - side_square // 2 > 0
         and cs[1] + side_square // 2 < canvas_size[1]
     )
-    get_center_square = lambda theta, ll: (
-        np.cos(theta) * ll + center_circle[0],
-        np.sin(theta) * ll + center_circle[1],
-    )
-
-    pbar = tqdm(total=config.num_samples, dynamic_ncols=True)
-    completed_samples = 0
 
     with open(output_folder / "annotation.csv", "w", newline="") as annfile:
         writer = csv.writer(annfile)
         writer.writerow(
             [
+                "SampleID",
+                "Condition",
+                "ManipulatedShape",
                 "Path",
-                "Type",
                 "BackgroundColor",
-                "TopShape",
                 "CenterCircleLocation",
                 "CenterSquareLocation",
                 "RadiusCircle",
@@ -173,54 +168,51 @@ def generate_all(config: AmodalCompletionConfig):
             ]
         )
 
+        pbar = tqdm(total=config.num_samples, dynamic_ncols=True)
+        completed_samples = 0
+
         while completed_samples < config.num_samples:
             radius_circle = random.randint(20, 40)
-            side_square = radius_circle * 1.5
+            side_square = np.ceil(radius_circle * 1.8).astype(int)
             diagonal_square = side_square * np.sqrt(2)
-            center_circle = np.array(canvas_size) // 2
-            theta = np.random.uniform(0, np.pi * 2)
 
-            ll = np.random.uniform(
-                diagonal_square / 2 + radius_circle,
-                np.sqrt(canvas_size[1] ** 2 + canvas_size[0] ** 2),
-            )
-            center_square = get_center_square(theta, ll)
-            if not check_square_fully_in_canvas(center_square):
-                continue
+            base_center_dist = (radius_circle + diagonal_square / 2)  # minimum distance between centers so that there is no overalap
+
+            while True:
+                # determine circle position
+                base_center_circle = np.random.uniform(0 + radius_circle, 224 - radius_circle, size=2).round().astype(int)
+
+                # trace position of the non occluding square along a random direction
+                theta = np.random.uniform(0, np.pi * 2)
+                center_square_dir = np.array([np.sin(theta), np.cos(theta)])
+
+                factor = np.random.uniform(1.05, 1.2) * base_center_dist # scale the distance between centers by a random value
+                base_center_square = np.ceil(base_center_circle + factor * center_square_dir).astype(int)
+
+                # if the non-occluding square is in the canvas, accept the image
+                if check_square_fully_in_canvas(base_center_square):
+                    break
+
             pbar.update(1)
 
-            for top_shape in top_shapes:
-                circle_col = (
-                    generate_random_color()
-                    if config.circle_color == "random"
-                    else config.circle_color
-                )
-                square_col = (
-                    generate_random_color()
-                    if config.square_color == "random"
-                    else config.square_color
-                )
-                img = ds.draw(
-                    center_circle,
-                    center_square,
-                    circle_col,
-                    square_col,
-                    radius_circle,
-                    side_square,
-                    center_notched=None,
-                    top=top_shape,
-                )
-                unique_hex = uuid.uuid4().hex[:8]
-                path = Path("no_occlusion") / f"{top_shape}_{unique_hex}.png"
-                img.save(output_folder / path)
+            # generate colors
+            circle_col = (
+                generate_random_color() if config.circle_color == "random" else config.circle_color
+            )
+            square_col = (
+                generate_random_color() if config.square_color == "random" else config.square_color
+            )
+
+            def write_row(path, condition, manipulated_shape, center_circle, center_square):
                 writer.writerow(
                     [
+                        completed_samples,
+                        condition,
+                        manipulated_shape,
                         path,
-                        "no_occlusion",
                         ds.background,
-                        top_shape,
-                        center_circle,
-                        center_square,
+                        [i.item() for i in center_circle],
+                        [i.item() for i in center_square],
                         radius_circle,
                         side_square,
                         completed_samples,
@@ -229,55 +221,98 @@ def generate_all(config: AmodalCompletionConfig):
                     ]
                 )
 
-            max_dist_occluded = vector_length(side_square, theta) + radius_circle * 0.8
-            ll = np.random.uniform(radius_circle // 1.2, max_dist_occluded)
+            def save_image(img, condition, top_shape):
+                unique_hex = uuid.uuid4().hex[:8]
+                name = (top_shape + '_') if condition != 'non_occluded' else ''
+                path = Path(condition) / f"{completed_samples}_{name}{unique_hex}.png"
+                img.save(output_folder / path)
+                return path
 
-            for notched in [True, False]:
-                for top_shape in top_shapes:
-                    center_circle = np.array(canvas_size) // 2
-                    center_square = get_center_square(theta, ll)
-                    if notched:
-                        if top_shape == "c":
-                            center_notched = center_circle
-                            center_circle = get_center_square(
-                                theta, -radius_circle * 0.4
-                            )
-                        else:
-                            center_notched = center_square
-                            center_square = get_center_square(theta, side_square * 1.4)
-                    else:
-                        center_notched = None
-                    img = ds.draw(
-                        center_circle,
-                        center_square,
-                        circle_col,
-                        square_col,
-                        radius_circle,
-                        side_square,
-                        center_notched=center_notched,
-                        top=top_shape,
-                    )
-                    unique_hex = uuid.uuid4().hex[:8]
-                    path = (
-                        Path("notched" if notched else "occlusion")
-                        / f"{completed_samples}_{top_shape}_{unique_hex}.png"
-                    )
-                    img.save(output_folder / path)
-                    writer.writerow(
-                        [
-                            path,
-                            "notched" if notched else "occlusion",
-                            ds.background,
-                            top_shape,
-                            center_circle,
-                            center_square,
-                            radius_circle,
-                            side_square,
-                            completed_samples,
-                            circle_col,
-                            square_col,
-                        ]
-                    )
+            # Generate base image
+            base_img = ds.draw(
+                base_center_circle,
+                base_center_square,
+                circle_col,
+                square_col,
+                radius_circle,
+                side_square,
+                center_notched=None,
+                top='s',
+            )
+            path = save_image(base_img, 'non_occluded', None)
+            write_row(path, 'non_occluded', None, base_center_circle, base_center_square)
+
+            # Generate conditions for the square-on-top-variant
+            # now determine the position of an occluded circle variant and generate it
+            factor = np.random.uniform(0.25, 0.75) * base_center_dist
+            center_occluding_square = np.ceil(
+                base_center_circle + factor * center_square_dir
+            ).astype(int)
+
+            circle_occluded_img = ds.draw(
+                base_center_circle,
+                center_occluding_square,
+                circle_col,
+                square_col,
+                radius_circle,
+                side_square,
+                center_notched=None,
+                top='s',
+            )
+            path = save_image(circle_occluded_img, 'occluded', 'circle')
+            write_row(path, 'occluded', 'square', base_center_circle, center_occluding_square)
+
+            # with the same information, we can generate the notched variant
+            circle_notched_img = ds.draw(
+                base_center_circle,
+                base_center_square,
+                circle_col,
+                square_col,
+                radius_circle,
+                side_square,
+                center_notched=center_occluding_square,
+                top='s',
+            )
+            path = save_image(circle_notched_img, 'notched', 'circle')
+            write_row(path, 'notched', 'square', base_center_circle, base_center_square)
+
+
+            # Generate conditions for the circle-on-top-variant
+            # determine the position and generate an occluded square variant
+            factor = np.random.uniform(0.25, 0.75) * base_center_dist
+            center_occluding_circle = np.ceil(
+                base_center_square - factor * center_square_dir
+            ).astype(int)
+
+            square_occluded_img = ds.draw(
+                center_occluding_circle,
+                base_center_square,
+                circle_col,
+                square_col,
+                radius_circle,
+                side_square,
+                center_notched=None,
+                top='c',
+            )
+            path = save_image(square_occluded_img, 'occluded', 'square')
+            write_row(path, 'occluded', 'square', center_occluding_circle, base_center_square)
+
+            square_notched_img = ds.draw(
+                base_center_circle,
+                base_center_square,
+                circle_col,
+                square_col,
+                radius_circle,
+                side_square,
+                center_notched=center_occluding_circle,
+                top='c',
+            )
+            path = save_image(square_notched_img, 'notched', 'square')
+            write_row(path, 'notched', 'square', base_center_circle, base_center_square)
+
+            # base_img.show()
+            # square_occluded_img.show()
+            # square_notched_img.show()
 
             completed_samples += 1
 
